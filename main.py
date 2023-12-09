677dd2688db7557b35bb30c019a3718a1a3230b9
Conrad Heinrich Carl <coheca0815@gmail.com>
2023-12-09 23:48:46 +0100

    support for simulation of multiple games

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
+import connectfour.utils as utils
 from agents.random_bot import bot as random_bot
+from connectfour.Agent import Agent
 from connectfour.ConnectFour import ConnectFour
 from connectfour.display import PygameDisplay
 
 
-def main():
-    agent1 = random_bot
-
-    env = ConnectFour(agent1, agent1)
-    display = PygameDisplay()
+def sim_game(agent1: Agent, agent2: Agent, render: bool = False) -> int:
+    env = ConnectFour(agent1, agent2)
+    if render:
+        display = PygameDisplay()
     while True:
         valid_move, win, player = env.step()
         if not valid_move:
-            print(f"Invalid move from player {player}")
-            break
+            return utils.opponent_mark(env.obs)
 
         if win:
-            print(f"Player {player} won!")
-            break
+            return player
+        if render:
+            display.render(env.obs, env.config)  # type: ignore
+            display.clock.tick(10)  # type:ignore
 
-        display.render(env.obs, env.config)
-        display.clock.tick(10)
 
-    while True:
-        display.clock.tick(10)
+def main(n_games: int, render=False):
+    agent1 = random_bot
+    agent2 = random_bot
+    who = []
+    for _ in range(n_games):
+        who.append(sim_game(agent1, agent2, render))
+
+    player_1_wins = who.count(1)
+    print(f"Player 1 won {player_1_wins} games ({player_1_wins / n_games * 100:.2f}%)")
+    print(
+        f"Player 2 won {n_games - player_1_wins} games ({(n_games - player_1_wins) / n_games * 100:.2f}%)"
+    )
 
 
 if __name__ == "__main__":
-    main()
+    main(100)
