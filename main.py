d546308e591d3741be8b58c02666f8238f55a0e9
Conrad Heinrich Carl <coheca0815@gmail.com>
2023-12-09 22:45:00 +0100

    simple main loop

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+from connect_four.ConnectFour import ConnectFour  # type:ignore
+
+from agents.random_bot import bot as random_bot
+
+
 def main():
-    pass
+    agent1 = random_bot
+
+    env = ConnectFour(agent1, agent1)
+
+    while True:
+        valid_move, win, player = env.step()
+        if not valid_move:
+            print(f"Invalid move from player {player}")
+            break
+
+        if win:
+            print(f"Player {player} won!")
+            break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
