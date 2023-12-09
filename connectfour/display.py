e896d59cd00e079bee7c33e69d9e0135004740aa
Conrad Heinrich Carl <coheca0815@gmail.com>
2023-12-09 23:21:02 +0100

    reduce screen size

--- connectfour/display.py
+++ connectfour/display.py
@@ -1,25 +1,24 @@
+import numpy as np
 import pygame  # type:ignore
 
 
 class PygameDisplay:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1280, 720))
+        self.screen = pygame.display.set_mode((480, 480))
         self.clock = pygame.time.Clock()
         self.running = True
 
-    def render(self, board, config):
-        board = board.reshape(config.rows, config.columns)
+    def render(self, obs, config):
+        board = np.asarray(obs.board).reshape(config.rows, config.columns)
         self.screen.fill((0, 0, 0))
         for row in range(config.rows):
-            for col in range(config.cols):
+            for col in range(config.columns):
                 if board[row][col] == 1:
-                    pygame.draw.circle(
-                        self.screen, (255, 0, 0), (col * 100 + 50, row * 100 + 50), 40
-                    )
+                    pygame.draw.circle(self.screen, (255, 0, 0), (col * 66 + 33, row * 66 + 33), 26)
                 elif board[row][col] == 2:
-                    pygame.draw.circle(
-                        self.screen, (0, 255, 0), (col * 100 + 50, row * 100 + 50), 40
-                    )
+                    pygame.draw.circle(self.screen, (0, 255, 0), (col * 66 + 33, row * 66 + 33), 26)
         pygame.display.flip()
-        self.clock.tick(60)
+
+    def draw_victory(self, obs, config):
+        board = np.asarray(obs.board).reshape(config.rows, config.columns)
