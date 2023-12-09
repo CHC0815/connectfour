a3989a31e5c9fce7467952ff7e71f1fa1ff153f3
Conrad Heinrich Carl <coheca0815@gmail.com>
2023-12-09 22:44:54 +0100

    add type annotation

--- connectfour/ConnectFour.py
+++ connectfour/ConnectFour.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import numpy as np
 
 from connectfour.utils import check_win, drop_piece, get_valid_moves
@@ -19,11 +21,11 @@ class ConnectFour:
         self.player = 1
         return self.obs
 
-    def step(self) -> bool:
+    def step(self) -> Tuple[bool, bool, int]:
         bot = self.player1 if self.player == 1 else self.player2
         col = bot(self.obs, self.config, self.player)
         if col not in get_valid_moves(self.obs, self.config):
-            raise ValueError("Invalid move")
+            return False, False, self.player
         grid = drop_piece(self.obs, col, self.player, self.config)
         self.obs = grid.flatten()
-        return check_win(self.obs, self.config, self.player)
+        return True, check_win(self.obs, self.config, self.player), self.player
