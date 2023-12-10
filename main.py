ad6d01e78ab7f9f33bfaa7fc8ad9fb92aec76cb4
Conrad H. Carl <coheca0815@gmail.com>
2023-12-10 14:15:18 +0100

    add command line argument for number of games

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import argparse
+
 import connectfour.utils as utils
 from agents.nstep import bot as nstep_bot
 from agents.onestep import bot as onestep_bot
@@ -5,14 +7,18 @@ from agents.random_bot import bot as random_bot
 from connectfour.Simulation import Simulation
 
 
-def main(n_games: int):
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--n-games", type=int, default=10, help="Number of games to play")
+    args = parser.parse_args()
+
     agent1 = random_bot
     agent2 = onestep_bot
 
-    sim = Simulation(agent1, agent2, n_games)
+    sim = Simulation(agent1, agent2, args.n_games)
     sim.run()
     sim.print_results()
 
 
 if __name__ == "__main__":
-    main(2400)
+    main()
