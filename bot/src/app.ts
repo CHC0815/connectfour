55aaae3ad4ddef049a959cc4028bb3b5efd70e82
Conrad Heinrich Carl <coheca0815@gmail.com>
2024-01-03 00:10:49 +0100

    add sqlite3 dependency

--- bot/src/app.ts
+++ bot/src/app.ts
@@ -1,57 +1,11 @@
-import { Update } from "@telegraf/types";
-import { Context, Markup, Telegraf, Telegram } from "telegraf";
+import { Context, Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
 
-const token: string = process.env.BOT_TOKEN as string;
-
-const telegram: Telegram = new Telegram(token);
-
-const bot: Telegraf<Context<Update>> = new Telegraf(token);
-
-const chatId: string = process.env.CHAT_ID as string;
-
-bot.start((ctx) => {
-  ctx.reply("Hello " + ctx.from.first_name + "!");
-});
-
-bot.help((ctx) => {
-  ctx.reply("Send /start to receive a greeting");
-  ctx.reply("Send /keyboard to receive a message with a keyboard");
-  ctx.reply("Send /quit to stop the bot");
-});
-
-bot.command("quit", (ctx) => {
-  // Explicit usage
-  ctx.telegram.leaveChat(ctx.message.chat.id);
-
-  // Context shortcut
-  ctx.leaveChat();
-});
-
-bot.command("keyboard", (ctx) => {
-  ctx.reply(
-    "Keyboard",
-    Markup.inlineKeyboard([
-      Markup.button.callback("First option", "first"),
-      Markup.button.callback("Second option", "second"),
-    ])
-  );
-});
-
-bot.on("text", (ctx) => {
-  ctx.reply(
-    "You choose the " +
-      (ctx.message.text === "first" ? "First" : "Second") +
-      " Option!"
-  );
-
-  if (chatId) {
-    telegram.sendMessage(
-      chatId,
-      "This message was sent without your interaction!"
-    );
-  }
-});
-
+const bot = new Telegraf(process.env.BOT_TOKEN || "");
+bot.start((ctx) => ctx.reply(`Welcome ${ctx.from.first_name}`));
+bot.help((ctx) => ctx.reply("Play a game of connect four"));
+bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
 bot.launch();
 
 // Enable graceful stop
